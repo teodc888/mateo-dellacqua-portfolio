52293e49c14670c2c66c99fb92b10fd9944bab0b
mateo <teodc4447@gmail.com>
2022-05-02 19:18:26 -0300

    agregando mensaje funcional a la pagina

--- src/components/contactar/contactar.jsx
+++ src/components/contactar/contactar.jsx
@@ -39,82 +39,103 @@ export default function Contactar() {
         >
           <Typed strings={["Contactar"]} typeSpeed={50} backSpeed={50} loop />
         </Typography>
-        <Box sx={{ flexGrow: 1 }}>
-          <Grid
-            container
-            spacing={{ xs: 4, md: 3 }}
-            columns={{ xs: 4, sm: 8, md: 16 }}
-          >
-            <Grid item xs={4} sm={4} md={8}>
-              <TextField
-                id="outlined-basic"
-                label="Nombre"
-                variant="outlined"
-                sx={{ width: "100%" }}
-              />
+        <form
+          action="https://formsubmit.co/teodellacqua8@gmail.com"
+          method="POST"
+        >
+          <Box sx={{ flexGrow: 1 }}>
+            <Grid
+              container
+              spacing={{ xs: 4, md: 3 }}
+              columns={{ xs: 4, sm: 8, md: 16 }}
+            >
+              <Grid item xs={4} sm={4} md={8}>
+                <TextField
+                  id="outlined-basic"
+                  label="Nombre"
+                  variant="outlined"
+                  sx={{ width: "100%" }}
+                  type="text"
+                  name="name"
+                  required
+                />
+              </Grid>
+              <Grid item xs={4} sm={4} md={8}>
+                <TextField
+                  id="outlined-basic"
+                  label="Apellido"
+                  variant="outlined"
+                  sx={{ width: "100%" }}
+                  type="lastname"
+                  name="lastname"
+                  required
+                />
+              </Grid>
+              <Grid item xs={4} sm={4} md={8}>
+                <TextField
+                  id="outlined-basic"
+                  label="Email"
+                  variant="outlined"
+                  sx={{ width: "100%" }}
+                  type="email"
+                  name="email"
+                  required
+                />
+              </Grid>
+              <Grid item xs={4} sm={4} md={8}>
+                <TextField
+                  id="outlined-basic"
+                  label="Asunto"
+                  variant="outlined"
+                  sx={{ width: "100%" }}
+                  type="business"
+                  name="business"
+                  required
+                />
+              </Grid>
+              <Grid item xs={4} sm={8} md={16}>
+                <TextareaAutosize
+                  aria-label="minimum height"
+                  minRows={3}
+                  placeholder="Mensaje"
+                  style={{
+                    width: "100%",
+                    height: "200px",
+                    background: "#212121",
+                    color: "white",
+                    borderRadius: "10px",
+                    padding: "10px",
+                  }}
+                  type="mensaje"
+                  name="mensaje"
+                  required
+                />
+              </Grid>
+              <Grid item xs={4} sm={4} md={16} sx={{ textAlign: "center" }}>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  sx={{
+                    width: { xs: "100%", sm: "30%", md: "20%", lg: "20%" },
+                    height: "50px",
+                    fontSize: "25px",
+                    color: "white",
+                    background:
+                      "linear-gradient(45deg, #0d47a1, #0d47a1, #0d47a1, #0d47a1, #0d47a1, #0d47a1)",
+                    borderRadius: "10px",
+                    boxShadow: "0px 0px 10px #0d47a1",
+                    transition: "all 0.3s ease-in-out",
+                    ":hover": { transform: "scale(1.1)" },
+                    mb: "2%",
+                  }}
+                  type="submit"
+                >
+                  Enviar
+                </Button>
+              </Grid>
             </Grid>
-            <Grid item xs={4} sm={4} md={8}>
-              <TextField
-                id="outlined-basic"
-                label="Apellido"
-                variant="outlined"
-                sx={{ width: "100%" }}
-              />
-            </Grid>
-            <Grid item xs={4} sm={4} md={8}>
-              <TextField
-                id="outlined-basic"
-                label="Email"
-                variant="outlined"
-                sx={{ width: "100%" }}
-              />
-            </Grid>
-            <Grid item xs={4} sm={4} md={8}>
-              <TextField
-                id="outlined-basic"
-                label="Asunto"
-                variant="outlined"
-                sx={{ width: "100%" }}
-              />
-            </Grid>
-            <Grid item xs={4} sm={8} md={16}>
-              <TextareaAutosize
-                aria-label="minimum height"
-                minRows={3}
-                placeholder="Mensaje"
-                style={{
-                  width: "100%",
-                  height: "200px",
-                  background: "#212121",
-                  color: "white",
-                  borderRadius: "10px",
-                  padding: "10px",
-                }}
-              />
-            </Grid>
-            <Grid item xs={4} sm={4} md={16} sx={{ textAlign: "center" }}>
-              <Button
-                variant="contained"
-                color="primary"
-                sx={{
-                  width: { xs: "100%", sm: "30%", md: "20%", lg: "20%" },
-                  height: "50px",
-                  fontSize: "25px",
-                  color: "white",
-                  background:
-                    "linear-gradient(45deg, #0d47a1, #0d47a1, #0d47a1, #0d47a1, #0d47a1, #0d47a1)",
-                  borderRadius: "10px",
-                  boxShadow: "0px 0px 10px #0d47a1",
-                  transition: "all 0.3s ease-in-out",
-                  ":hover": { transform: "scale(1.1)" },
-                  mb: "2%",
-                }}
-              >
-                Enviar
-              </Button>
-            </Grid>
-          </Grid>
-        </Box>
+          </Box>
+        </form>
       </Stack>
     </Box>
   );
