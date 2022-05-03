4aebbe7d2263805e9273203b1c3c7cb8ab465486
mateo <teodc4447@gmail.com>
2022-05-03 17:32:51 -0300

    container

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -34,48 +34,49 @@ export default function Home() {
   };
 
   return (
-    <Container maxWidth="L">
-      <br id="quienSoy" />
-      <Box
-        sx={{
-          marginTop: { xs: "150px", sm: "210px", md: "210px", lg: "200px" },
-        }}
-      >
-        <QuienSoy />
-      </Box>
-      <br id="conocimientos" />
-      <Box
-        sx={{
-          marginTop: { xs: "250px", sm: "210px", md: "210px", lg: "200px" },
-        }}
-      >
-        <Conocimientos />
-      </Box>
-      <br id="experiencia" />
-      <Box
-        sx={{
-          marginTop: { xs: "250px", sm: "210px", md: "210px", lg: "200px" },
-        }}
-      >
-        <Experiencia />
-      </Box>
-      <br id="proyectos" />
-      <Box
-        sx={{
-          marginTop: { xs: "250px", sm: "210px", md: "210px", lg: "200px" },
-        }}
-      >
-        <Proyectos />
-      </Box>
-      <br id="contactar" />
-      <Box
-        sx={{
-          marginTop: { xs: "250px", sm: "210px", md: "210px", lg: "200px" },
-        }}
-      >
-        <Contactar />
-      </Box>
-      <br />
+    <>
+      <Container maxWidth="L">
+        <br id="quienSoy" />
+        <Box
+          sx={{
+            marginTop: { xs: "150px", sm: "210px", md: "210px", lg: "200px" },
+          }}
+        >
+          <QuienSoy />
+        </Box>
+        <br id="conocimientos" />
+        <Box
+          sx={{
+            marginTop: { xs: "250px", sm: "210px", md: "210px", lg: "200px" },
+          }}
+        >
+          <Conocimientos />
+        </Box>
+        <br id="experiencia" />
+        <Box
+          sx={{
+            marginTop: { xs: "250px", sm: "210px", md: "210px", lg: "200px" },
+          }}
+        >
+          <Experiencia />
+        </Box>
+        <br id="proyectos" />
+        <Box
+          sx={{
+            marginTop: { xs: "250px", sm: "210px", md: "210px", lg: "200px" },
+          }}
+        >
+          <Proyectos />
+        </Box>
+        <br id="contactar" />
+        <Box
+          sx={{
+            marginTop: { xs: "250px", sm: "210px", md: "210px", lg: "200px" },
+          }}
+        >
+          <Contactar />
+        </Box>
+      </Container>
       <Box
         sx={{
           marginTop: { xs: "250px", sm: "210px", md: "210px", lg: "200px" },
@@ -102,6 +103,6 @@ export default function Home() {
           <ArrowUpwardIcon />
         </Fab>
       )}
-    </Container>
+    </>
   );
 }
